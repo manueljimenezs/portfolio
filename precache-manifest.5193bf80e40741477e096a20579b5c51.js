f33f84954828541e309b6ae686a6350cf5dfe5e7
Manu <manujs@outlook.com>
2020-06-28 02:18:00 +0200

    Updates

---
+++ precache-manifest.5193bf80e40741477e096a20579b5c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f9d718b1bbf55250c0742fe486a761",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "51b8e2b0126cebe72b3a",
+    "url": "/portfolio/static/css/main.6034167f.chunk.css"
+  },
+  {
+    "revision": "ccf87c0451c2cf94a931",
+    "url": "/portfolio/static/js/2.a438326b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.a438326b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51b8e2b0126cebe72b3a",
+    "url": "/portfolio/static/js/main.e5924674.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "78e4a9c25861374066235255d11481ef",
+    "url": "/portfolio/static/media/me.78e4a9c2.svg"
+  }
+]);
