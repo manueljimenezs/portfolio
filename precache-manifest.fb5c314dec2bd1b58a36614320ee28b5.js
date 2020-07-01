c12083b7d4640b3396f126fe660ad25db6e3eb3a
Manu <manujs@outlook.com>
2020-07-01 12:45:08 +0200

    Updates

---
+++ precache-manifest.fb5c314dec2bd1b58a36614320ee28b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "890cb7d437034c5272f5bce892bce511",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c172bc7db14073d6e6a0",
+    "url": "/portfolio/static/css/main.3b6eb30b.chunk.css"
+  },
+  {
+    "revision": "671494ae214729d00a18",
+    "url": "/portfolio/static/js/2.64406865.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.64406865.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c172bc7db14073d6e6a0",
+    "url": "/portfolio/static/js/main.727b04ff.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  },
+  {
+    "revision": "78e4a9c25861374066235255d11481ef",
+    "url": "/portfolio/static/media/me.78e4a9c2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.40b88d64003dad4aa7032ebd1ddbc498.js"
+  "/portfolio/precache-manifest.fb5c314dec2bd1b58a36614320ee28b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
